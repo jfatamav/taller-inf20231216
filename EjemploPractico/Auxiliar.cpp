3289879d364326b0ed006c90e6b06099c607bff3
jfatamav <jfatamav@aeditip.com>
2023-12-16 11:49:08 -0500

    Código base para ejercicio

---
+++ EjemploPractico/Auxiliar.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.cpp
+ * Author: JPato
+ *
+ * Created on 16 de diciembre de 2023, 11:38
+ */
+
+#include "Auxiliar.h"
+
+void imprimirLinea(char caracter, int repeticiones){
+    for(int i=0; i<repeticiones; i++)
+        cout << caracter;
+    cout << endl;
+}
---
+++ EjemploPractico/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: JPato
+ *
+ * Created on 16 de diciembre de 2023, 11:35
+ */
+
+#include "Auxiliar.h"
+
+int main(int argc, char** argv) {
+    cout << "Bienvenidos al Taller de Git y GitHub :)" << endl;
+    imprimirLinea('=', 50);
+    cout << "Mensajes de los asistentes" << endl;
+    // COLOCAR AQUÍ LA INVOCACIÓN A SUS FUNCIONES
+    return 0;
+}
+
